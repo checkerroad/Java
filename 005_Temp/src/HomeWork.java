dff7d14d4730955acff02a8502f988074e418568
checkerroad <34999302+checkerroad@users.noreply.github.com>
2024-05-19 07:37:36 +0300

    Add files via upload

---
+++ 005_Temp/src/HomeWork.java
@@ -0,0 +1,12 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int cash = 9;
+        if (cash  > 500) {
+            System.out.println("Pizza");
+        } else if (cash < 100 & cash > 20) {
+            System.out.println("Doshirak");
+        } else  {
+            System.out.println("Hunger");
+        }
+    }
+}
